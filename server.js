79ef189a9a05a8f758ce6b9d7d3455fc12aae0c3
Vlad-R-CPC <pochva2024@gmail.com>
2026-05-15 15:21:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3737;
@@ -27,6 +28,7 @@ fs.mkdirSync(TRANSFER_DIR, { recursive: true });
 app.use(cors());
 app.use(express.json({ limit: "8mb" }));
 app.use(express.urlencoded({ extended: true, limit: "8mb" }));
+app.use('/portal', express.static(path.join(__dirname, 'public/portal')));
 
 let participants = [];
 let signals = [];
